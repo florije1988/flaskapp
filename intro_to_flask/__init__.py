dbbc8e522e73249e8c5ee23cb5601682fa45c32a
florije1988 <florije1988@gmail.com>
2015-09-02 16:16:26 +0800

    Update: 文件已经补全，稍后测试。

--- intro_to_flask/__init__.py
+++ intro_to_flask/__init__.py
@@ -1,2 +1,26 @@
 # -*- coding: utf-8 -*-
 __author__ = 'florije'
+
+from flask import Flask
+
+app = Flask(__name__)
+
+app.secret_key = 'development key'
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'contact@example.com'
+app.config["MAIL_PASSWORD"] = 'your-password'
+
+from routes import mail
+
+mail.init_app(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://your-username:your-password@localhost/development'
+
+from models import db
+
+db.init_app(app)
+
+import intro_to_flask.routes
